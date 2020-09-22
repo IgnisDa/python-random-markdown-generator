05638943bd04863c42d6f1f8cc09b1e71d1280c0
IgnisDa <ignisda2002@gmail.com>
2020-09-22 16:11:07 +0530

    Fix flake8 config, remove tox from testing

--- mdgen/markdown.py
+++ mdgen/markdown.py
@@ -5,10 +5,16 @@ from mdgen.base.text import MarkdownTextGenerator
 class MarkdownGenerator:
 
     def new_text(self, text: str = None):
+        if not isinstance(text, (str, int)):
+            raise ValueError(f"`text` must be an instance of {str} or {int}")
         output = MarkdownTextGenerator(text)
         return output.new_text()
 
     def new_header(self, text: str = None, header_level: int = None,
                    linebreak=True):
+        if not isinstance(text, (str, int)):
+            raise ValueError(f"`text` must be an instance of {str} or {int}")
+        if not isinstance(header_level, (int,)):
+            raise ValueError(f"`header_level` must be an instance of or {int}")
         output = MarkdownHeaderGenerator(text, header_level, linebreak)
         return output.new_header()
