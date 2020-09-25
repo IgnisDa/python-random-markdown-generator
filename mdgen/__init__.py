7c92fdfb123cc5b7a6aef7d1b6739fc41c502fed
IgnisDa <ignisda2002@gmail.com>
2020-09-25 12:53:47 +0530

    New travis config to deploy to testpypi. Bump version.

--- mdgen/__init__.py
+++ mdgen/__init__.py
@@ -1,7 +1,7 @@
 from mdgen.main import MarkdownOutputGenerator
 from mdgen.markdown import MarkdownGenerator
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [
     MarkdownGenerator, MarkdownOutputGenerator
