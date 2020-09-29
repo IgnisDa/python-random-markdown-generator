da390a97c74cd1e64efef6e8de45e99723984cb4
IgnisDa <ignisda2002@gmail.com>
2020-09-29 14:27:46 +0530

    Fix travis config #6

--- mdgen/__init__.py
+++ mdgen/__init__.py
@@ -2,7 +2,7 @@ from mdgen.generator import MarkdownPostGenerator
 from mdgen.main import MarkdownOutputGenerator
 from mdgen.markdown import MarkdownGenerator
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = [
     'MarkdownGenerator', 'MarkdownOutputGenerator', 'MarkdownPostGenerator'
