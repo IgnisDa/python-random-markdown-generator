4be078773aa4d0f152610a6a0649b74967aaa46e
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2020-10-04 13:48:34 +0530

    Create blockquote.py
    A class which aims to convert a string into a blockquote.

---
+++ mdgen/core/blockquote.py
@@ -0,0 +1,10 @@
+class MarkdownBlockQuoteGenerator:
+
+""" This class converts an input string into a markdown blockquote by adding '>' in the beginning of the string """
+    
+    def new_blockquote(self, quote:str):
+    
+    """ This is the method used to achieve what this class tends to"""
+    
+        quote = "> " + quote
+        return quote
