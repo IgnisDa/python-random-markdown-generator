cd02ff0991cd35d7bac8455298a7eb676efc46eb
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2020-10-05 13:38:31 +0530

    Update blockquote.py

--- mdgen/core/blockquote.py
+++ mdgen/core/blockquote.py
@@ -1,9 +1,9 @@
 class MarkdownBlockQuoteGenerator:
-    
+
     """This class converts an input string into a markdown
     blockquote by adding '>' in the beginning
     of the string"""
-    
+
     def new_blockquote(self, quote: str):
         """This is the method used to achieve what this class tends to"""
         quote = "> " + quote
