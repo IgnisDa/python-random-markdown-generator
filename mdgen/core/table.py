91b170eda47796f84c82a0c95059d1d7a8513d23
AXD <34991385+Agzo01@users.noreply.github.com>
2022-06-30 11:07:37 +0100

    Add newline before table.

--- mdgen/core/table.py
+++ mdgen/core/table.py
@@ -6,7 +6,7 @@ class MarkdownTableGenerator:
     """ This will create markdown tables using a list of lists. """
 
     def new_table(self, list_items_list: list):
-        output = ''
+        output = '\n '
         for index, list_item in enumerate(list_items_list):
             output += (
                 f"{MARKDOWN_TABLE_COL_SEPARATOR}"
